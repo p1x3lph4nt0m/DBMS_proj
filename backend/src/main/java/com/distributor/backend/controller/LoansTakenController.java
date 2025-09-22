da993dc401e59f14601c6f1dde96e049726485f9
Lakshya Vats <146511679+p1x3lph4nt0m@users.noreply.github.com>
2025-09-22 20:09:47 +0530

    Added Api 13

--- backend/src/main/java/com/distributor/backend/controller/LoansTakenController.java
+++ backend/src/main/java/com/distributor/backend/controller/LoansTakenController.java
@@ -5,10 +5,9 @@ import com.distributor.backend.service.LoansTakenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -19,5 +18,11 @@ public class LoansTakenController {
     public ResponseEntity<LoansTakenDto> addLoansTaken(@RequestBody LoansTakenDto loansTakenDto){
         LoansTakenDto savedLoan = loansTakenService.takeLoan(loansTakenDto);
         return new ResponseEntity<>(savedLoan, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<LoansTakenDto>> allLoansTaken(){
+        List<LoansTakenDto> allLoans = loansTakenService.allLoans();
+        return new ResponseEntity<>(allLoans,HttpStatus.CREATED);
     }
 }
--- backend/src/main/java/com/distributor/backend/service/impl/LoansTakenServiceImpl.java
+++ backend/src/main/java/com/distributor/backend/service/impl/LoansTakenServiceImpl.java
@@ -4,11 +4,15 @@ import com.distributor.backend.dto.LoansTakenDto;
 import com.distributor.backend.entity.LoansTaken;
 import com.distributor.backend.entity.Supplier;
 import com.distributor.backend.exception.ResourceNotFoundException;
+import com.distributor.backend.mapper.ItemsMapper;
 import com.distributor.backend.mapper.LoansTakenMapper;
 import com.distributor.backend.repository.LoansTakenRepository;
 import com.distributor.backend.repository.SupplierRepository;
 import com.distributor.backend.service.LoansTakenService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LoansTakenServiceImpl implements LoansTakenService {
@@ -38,5 +42,14 @@ public class LoansTakenServiceImpl implements LoansTakenService {
         LoansTaken savedLoan = loansTakenRepository.save(loan);
         return LoansTakenMapper.maptoLoansTakenDto(savedLoan);
     }
+
+    @Override
+    public List<LoansTakenDto> allLoans() {
+        return loansTakenRepository.findAll()
+                .stream()
+                .map(LoansTakenMapper::maptoLoansTakenDto)
+                .collect(Collectors.toList());
+    }
+
 
 }
