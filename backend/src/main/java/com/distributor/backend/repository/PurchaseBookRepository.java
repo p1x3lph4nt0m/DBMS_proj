9f97645a2e1788f062819772036df79d6447e445
Saksham Puri <sp1810627@gmail.com>
2025-09-25 14:09:12 +0530

    Changed Queries in PurchaseBook repo and SalesBook repo

--- backend/src/main/java/com/distributor/backend/repository/PurchaseBookRepository.java
+++ backend/src/main/java/com/distributor/backend/repository/PurchaseBookRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PurchaseBookRepository extends JpaRepository<PurchaseBook,Long> {
-    @Query(value = "SELECT * FROM purchase_book pb " + "JOIN supplier s ON pb.provider = s.gst " + "WHERE STR_TO_DATE(CONCAT(pb.order_year, '-', LPAD(pb.order_month, 2, '0'), '-', LPAD(pb.order_day, 2, '0')), '%Y-%m-%d') " + "BETWEEN :startDate AND :endDate " + "AND s.gst LIKE CONCAT(:gstPrefix, '%')", nativeQuery = true)
+    @Query(value = "SELECT * FROM purchase_book pb " + "WHERE STR_TO_DATE(CONCAT(pb.order_year, '-', LPAD(pb.order_month, 2, '0'), '-', LPAD(pb.order_day, 2, '0')), '%Y-%m-%d') " + "BETWEEN :startDate AND :endDate " + "AND pb.provider LIKE CONCAT(:gstPrefix, '%')", nativeQuery = true)
     List<PurchaseBook> findInDateRangeWithGstNative(@Param("startDate") String startDate, @Param("endDate") String endDate, @Param("gstPrefix") String gstPrefix);
 
 
--- backend/src/main/java/com/distributor/backend/repository/SalesBookRepository.java
+++ backend/src/main/java/com/distributor/backend/repository/SalesBookRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface SalesBookRepository extends JpaRepository<SalesBook,Long> {
-    @Query(value = "SELECT * FROM sales_book sb " + "JOIN buyer b ON sb.customer = b.gst " + "WHERE STR_TO_DATE(CONCAT(sb.order_year, '-', LPAD(sb.order_month, 2, '0'), '-', LPAD(sb.order_day, 2, '0')), '%Y-%m-%d') " + "BETWEEN :startDate AND :endDate " + "AND b.gst LIKE CONCAT(:gstPrefix, '%')", nativeQuery = true)
+    @Query(value = "SELECT * FROM sales_book sb " + "WHERE STR_TO_DATE(CONCAT(sb.order_year, '-', LPAD(sb.order_month, 2, '0'), '-', LPAD(sb.order_day, 2, '0')), '%Y-%m-%d') " + "BETWEEN :startDate AND :endDate " + "AND sb.customer LIKE CONCAT(:gstPrefix, '%')", nativeQuery = true)
     List<SalesBook> findInDateRangeWithGstNative(@Param("startDate") String startDate, @Param("endDate") String endDate, @Param("gstPrefix") String gstPrefix);
 }
     
